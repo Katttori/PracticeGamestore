ddeb71d76cf22ff5594ea923f037de05e8f19906
Danya Gavrylovskyi <danya.gavrilovskiy28@gmail.com>
2025-05-15 17:02:24 +0300

    fix: remove age restricting property in Genre entity

--- PracticeGamestore/PracticeGamestore.DataAccess/Entities/Genre.cs
+++ PracticeGamestore/PracticeGamestore.DataAccess/Entities/Genre.cs
@@ -6,5 +6,4 @@ public class Genre
     public string Name { get; set; }
     public Guid? ParentId { get; set; }
     public string Description { get; set; }
-    public int MinimumAgeRating { get; set; }
 }
