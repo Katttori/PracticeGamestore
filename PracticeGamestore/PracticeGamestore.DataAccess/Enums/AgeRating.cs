2b4afea7aa1b96db94718c31ca1fd5eefb39c9a0
MarharytaFilipovych <150688071+MarharytaFilipovych@users.noreply.github.com>
2025-05-15 16:55:34 +0300

    renamed and added numeric values to AgeRating

---
+++ PracticeGamestore/PracticeGamestore.DataAccess/Enums/AgeRating.cs
@@ -0,0 +1,10 @@
+namespace PracticeGamestore.DataAccess.Enums;
+
+public enum AgeCategory
+{
+    ThreePlus = 3,
+    SevenPlus = 7,
+    TwelvePlus = 12,
+    SixteenPlus = 16,
+    EighteenPlus = 18
+}
