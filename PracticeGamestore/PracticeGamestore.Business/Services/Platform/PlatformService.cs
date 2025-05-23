73036deb73501562dce45c425f5128d9783c3a6b
Kseniia Kravchenko <kkravchenko@kse.org.ua>
2025-05-23 15:42:31 +0300

    refactor: streamline PlatformService by removing private fields and using constructor parameters directly

--- PracticeGamestore/PracticeGamestore.Business/Services/Platform/PlatformService.cs
+++ PracticeGamestore/PracticeGamestore.Business/Services/Platform/PlatformService.cs
@@ -5,54 +5,46 @@ using PracticeGamestore.DataAccess.UnitOfWork;
 
 namespace PracticeGamestore.Business.Services.Platform;
 
-public class PlatformService : IPlatformService
+public class PlatformService(IPlatformRepository repository, IUnitOfWork unitOfWork) : IPlatformService
 {
-    private readonly IPlatformRepository _repository;
-    private readonly IUnitOfWork _unitOfWork;
-    
-    public PlatformService(IPlatformRepository repository, IUnitOfWork unitOfWork)
-    {
-        _repository = repository;
-        _unitOfWork = unitOfWork;
-    }
     
     public async Task<IEnumerable<PlatformDto>> GetAllAsync()
     {
-        var platforms = await _repository.GetAllAsync();
+        var platforms = await repository.GetAllAsync();
         return platforms.Select(p => p.MapToDto());
     }
     
     public async Task<PlatformDto?> GetByIdAsync(Guid id)
     {
-        var platform = await _repository.GetByIdAsync(id);
+        var platform = await repository.GetByIdAsync(id);
         return platform?.MapToDto();
     }
     
     public async Task<Guid?> CreateAsync(PlatformDto platform)
     {
         var entity = platform.MapToEntity();
-        var id = await _repository.CreateAsync(entity);
-        var changes = await _unitOfWork.SaveChangesAsync();
+        var id = await repository.CreateAsync(entity);
+        var changes = await unitOfWork.SaveChangesAsync();
         
         return changes > 0 ? entity.Id : null;
     }
     
     public async Task<bool> UpdateAsync(PlatformDto platform)
     {
-        var updatedPlatform = await _repository.GetByIdAsync(platform.Id);
+        var updatedPlatform = await repository.GetByIdAsync(platform.Id);
         if (updatedPlatform is null) return false;
         
         updatedPlatform.Name = platform.Name;
         updatedPlatform.Description = platform.Description;
         
-        _repository.Update(updatedPlatform);
-        var changes = await _unitOfWork.SaveChangesAsync();
+        repository.Update(updatedPlatform);
+        var changes = await unitOfWork.SaveChangesAsync();
         return changes > 0;
     }
     
     public async Task DeleteAsync(Guid id)
     {
-        await _repository.DeleteAsync(id);
-        await _unitOfWork.SaveChangesAsync();
+        await repository.DeleteAsync(id);
+        await unitOfWork.SaveChangesAsync();
     }
 }
