a757d9fe00c9ff908c66e7111f95d9ac16f29680
Kseniia Kravchenko <kkravchenko@kse.org.ua>
2025-05-27 22:32:13 +0300

    Revert "refactor: update mapping logic to conditionally set Id in Platform entity"
    This reverts commit 62ea914de1edd89ae774d4f8699b64e2117f9c04.

--- PracticeGamestore/PracticeGamestore.Business/Mappers/PlatformMappingExtensions.cs
+++ PracticeGamestore/PracticeGamestore.Business/Mappers/PlatformMappingExtensions.cs
@@ -6,17 +6,6 @@ public static class PlatformMappingExtensions
 {
     public static Platform MapToEntity(this PlatformDto dto)
     {
-        // If Id is not null, set it; otherwise, let the database generate it
-        if (dto.Id.HasValue)
-        {
-            return new Platform
-            {
-                Id = dto.Id.Value,
-                Name = dto.Name,
-                Description = dto.Description
-            };
-        }
-        
         return new Platform
         {
             Name = dto.Name,
