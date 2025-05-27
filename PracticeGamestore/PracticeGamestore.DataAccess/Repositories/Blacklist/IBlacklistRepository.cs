7c0f481bf22fe2384e33cef20915a7dd2b9945a3
Kseniia Kravchenko <kkravchenko@kse.org.ua>
2025-05-27 23:15:25 +0300

    feat: implement BlacklistRepository and IBlacklistRepository for CRUD operations

---
+++ PracticeGamestore/PracticeGamestore.DataAccess/Repositories/Blacklist/IBlacklistRepository.cs
@@ -0,0 +1,10 @@
+namespace PracticeGamestore.DataAccess.Repositories.Blacklist;
+
+public interface IBlacklistRepository
+{
+    Task<IEnumerable<Entities.Blacklist>> GetAllAsync();
+    Task<Entities.Blacklist?> GetByIdAsync(Guid id);
+    Task<Guid> CreateAsync(Entities.Blacklist blacklist);
+    void Update(Entities.Blacklist blacklist);
+    Task DeleteAsync(Guid id);
+}
