e885183ffa1a7aa2e0b21f3c30502084fbad360e
KravchenkoKseniia <kkravchenko@kse.org.ua>
2025-06-13 16:15:22 +0300

    fix: add game validation in file upload process of FileService

--- PracticeGamestore/PracticeGamestore.Business/Services/File/FileService.cs
+++ PracticeGamestore/PracticeGamestore.Business/Services/File/FileService.cs
@@ -5,12 +5,14 @@ using Microsoft.Extensions.Logging;
 using PracticeGamestore.Business.DataTransferObjects;
 using PracticeGamestore.Business.Mappers;
 using PracticeGamestore.DataAccess.Repositories.File;
+using PracticeGamestore.DataAccess.Repositories.Game;
 using PracticeGamestore.DataAccess.UnitOfWork;
 
 namespace PracticeGamestore.Business.Services.File;
 
 public class FileService(
     IFileRepository fileRepository,
+    IGameRepository gameRepository,
     IUnitOfWork unitOfWork,
     IWebHostEnvironment env,
     IConfiguration config,
@@ -33,6 +35,14 @@ public class FileService(
     
     public async Task<Guid?> UploadAsync(FileDto fileDto)
     {   
+        
+        var game = await gameRepository.GetByIdAsync(fileDto.GameId);
+        if (game == null)
+        {
+            logger.LogError("Game with id: {GameId} not found.", fileDto.GameId);
+            return null;
+        }
+        
         await unitOfWork.BeginTransactionAsync();
         var path = await SavePhysicalFileAsync(fileDto.File);
             
